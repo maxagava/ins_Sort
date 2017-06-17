c56b5671ad355328014c016dfdef93f85f1c8aaa
maxagava <maxagavaa@gmail.com>
2017-06-17 10:33:30 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,16 +1,22 @@
-#include <BinaryTree.hpp>
 #include <catch.hpp>
+#include "insertion_sort.h"
 
-SCENARIO("sort","[sort]")
-{
- 	int matrix[6] = { 222 , 111, 666, 333, 555, 444 };
-	int sorted_matrix[6] =  { 111, 222, 333, 444, 555, 666 };
-	int* first = &matrix[0];
-	int* second = &matrix[6];
-	insertion_sort(first, second);
-	bool check = NULL;
-	for (int i = 0; i < 6; i++)
-	if (matrix[i] == sorted_matrix[i])
-	check = true;
-	REQUIRE(check == true);
+SCENARIO("test by vector", "[vector]") {
+	std::vector <int> v;
+	v.push_back(8);
+	v.push_back(1);
+	v.push_back(5);
+	v.push_back(9);
+	v.push_back(4);
+	REQUIRE(v[0] == 8);
+	REQUIRE(v[1] == 1);
+	REQUIRE(v[2] == 5);
+	REQUIRE(v[3] == 9);
+	REQUIRE(v[4] == 4);
+	insertion_sort(v.begin(), v.end());
+	REQUIRE(v[0] == 1);
+	REQUIRE(v[1] == 4);
+	REQUIRE(v[2] == 5);
+	REQUIRE(v[3] == 8);
+	REQUIRE(v[4] == 9);
 }
